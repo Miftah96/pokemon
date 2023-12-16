6e68f0c4c582c65eef15779e7cc86b56263cc8b7
Akhmad Miftah <akhmad.riyadhi@anabatic.com>
2023-12-16 16:33:07 +0700

    [all] Switch primeFaces to tailwindCSS

--- src/main.js
+++ src/main.js
@@ -1,28 +1,11 @@
-import "primeflex/primeflex.css";
-import "primevue/resources/themes/lara-light-green/theme.css";
-import "primevue/resources/primevue.min.css"; /* Deprecated */
-import "primeicons/primeicons.css";
+import "./assets/style.css";
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import PrimeVue from 'primevue/config';
-
-import Button from 'primevue/button';
-import DataView from 'primevue/dataview';
-import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions';
-import Rating from 'primevue/rating';
-import Tag from 'primevue/tag';
 
 const app = createApp(App);
 
 app.use(router)
-app.use(PrimeVue);
-
-app.component('Button', Button);
-app.component('DataView', DataView);
-app.component('DataViewLayoutOptions', DataViewLayoutOptions);
-app.component('Rating', Rating);
-app.component('Tag', Tag);
 
 app.mount('#app')
--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import ThePokemonList from '../components/ThePokemonList.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <ThePokemonList />
   </main>
 </template>
